5dcf9057082a70ccdb801b69712965d0ba905b63
EliWilkinson <eli.wilkinson9@gmail.com>
2022-02-21 17:03:03 -0600

    defined __name__ and __main__

--- QuoteTracker.py
+++ QuoteTracker.py
@@ -46,9 +46,68 @@ def getStockValue(symbol):
 		else:
 			return [xmlDict.get('Name'), float(xmlDict.get('LastPrice'))]
 
-		except:
-			print("Corrupted Data")
-			return None
+	except:
+		print("Corrupted Data")
+		return None
+
+if __name__ == '__main__':
+
+	minInterval = 30
+	defInterval = minInterval
+	MINUTES_TO_SECONDS= 60
+
+	parser = arg.parse.ArgumentParser(description= 'Get value of stock with regular interval')
+	parser.add_argument("company_symbol",
+		help = "Specify the symbol name of company")
+	parser.add_argument("-i", "--interval", type=check_negative,
+		help= "Interval for regular checking in minutes. Default values is {}. Minimum vlaue is {}",format(defInterval, minInterval))
+
+	args = parser.parse_args()
+
+	companySymbol = args.company_symbol
+
+	if args.interval:
+		interval = int(args.interval)
+	else:
+		interval = defInterval
+
+	if interval < minInterval:
+		interval = minInterval
+
+	print("")
+	print("Getting stock of {} eith regular interval {} minutes".format(companySymbol, interval))
+	print("")
+
+	lastStockValue = 0
+	firstTime = True
+
+	while True:
+		currStockValue = getStockValue(companySymbol)
+
+		if currStockValueis None:
+			print("error Happening...")
+			print("program Quitting")
+			break
+		else:
+			if currStockValueis[1] < lastStockValue:
+				status= "[DOWN]"
+			elif currStockValue[1] > lastStockValue:
+				status = "[UP]"
+			else:
+				status = "[EQUAL"
+
+			nowStr = time.strftime('%Y-%m-%d %H: %M')
+
+			if firstTime:
+				status = ""
+				firstTime = False
+
+			print("{} | {} | {}".format(nowStr, currStockValue[1], status))
+
+			lastStockValue = currStockValue[1]
+
+		time.sleep(interval * MINUTES_TO_SECONDS)
+
 
 
 
